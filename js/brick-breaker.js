597af178d53a6ffbcb26ee980a0678b3363d9102
Keniran <naani.navis@gmail.com>
2020-02-19 17:03:35 -0500

    made paddle move after being stupid for a while

--- js/brick-breaker.js
+++ js/brick-breaker.js
@@ -1,4 +1,4 @@
-// Canvas
+// Cached Element References
 const canvas = document.getElementById("brick-breaker-game");
 const ctx = canvas.getContext("2d");
 
@@ -6,9 +6,12 @@ const ctx = canvas.getContext("2d");
 // Variables
 let dx;
 let xy;
-let speed = 15;
+let speed = 20;
+
 
 // Objects
+let bottles = [];
+
 let ball = {
     x: undefined,
     y: undefined,
@@ -17,14 +20,24 @@ let ball = {
     up: true
 };
 
+let paddle = {
+    x: (canvas.width / 2) - 30,
+    y: canvas.height - 10,
+    width: 60,
+    height: 5
+};
 
-// Functions
+
+// Event Listeners
 window.onload = function() {
-    document.getElementById("brick-breaker-game").onclick = reset;
+    document.getElementById("brick-breaker-game").onclick = init;
     game();
 }
+document.addEventListener("keydown", movePaddle);
 
-function reset() {
+
+// Functions
+function init() {
     ball.x = canvas.width / 2;
     ball.y = canvas.height - 20;
     ball.right = true;
@@ -50,6 +63,9 @@ function checkCollision() {
     if (ball.y - 10 <= 0) {
         ball.up = false;
     }
+    if (ball.y - 10 >= canvas.height) {
+        gameOver();
+    }
 }
 
 function changeXAndY() {
@@ -70,11 +86,22 @@ function changeXAndY() {
 }
 
 function draw() {
+    ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+
     ctx.beginPath();
     ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
     ctx.stroke();
 }
 
-function gameOver() {
+function movePaddle(event) {
+    if (event.keyCode == 37) {
+        paddle.x -= 10;
+    }
+    else if (event.keyCode == 39) {
+        paddle.x += 10;
+    }
+}
 
+function gameOver() {
+    init();
 }
