cd30b45d252c3085ad20903e314d404ed73bdf2e
Keniran <naani.navis@gmail.com>
2020-02-19 15:56:49 -0500

    added collision

--- js/brick-breaker.js
+++ js/brick-breaker.js
@@ -1,58 +1,80 @@
+// Canvas
 const canvas = document.getElementById("brick-breaker-game");
 const ctx = canvas.getContext("2d");
+
+
+// Variables
 let dx;
 let xy;
-let x;
-let y;
-let right = true;
-let up = true;
 let speed = 15;
 
+// Objects
+let ball = {
+    x: undefined,
+    y: undefined,
+    radius: 10,
+    right: true,
+    up: true
+};
+
+
+// Functions
 window.onload = function() {
     document.getElementById("brick-breaker-game").onclick = reset;
     game();
 }
 
 function reset() {
-    x = 375;
-    y = 490;
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height - 20;
+    ball.right = true;
+    ball.up = true;
 }
 
 function game() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     draw();
+    checkCollision();
     changeXAndY();
 
     setTimeout(game, speed);
 }
 
-function init() {
-
-}
-
 function checkCollision() {
-
+    if (ball.x - 10 <= 0) {
+        ball.right = true;
+    }
+    if (ball.x + 10 >= canvas.width) {
+        ball.right = false;
+    }
+    if (ball.y - 10 <= 0) {
+        ball.up = false;
+    }
 }
 
 function changeXAndY() {
-    if (right) {
+    if (ball.right) {
         dx = 5;
     }
     else {
         dx = -5;
     }
-    if (up) {
+    if (ball.up) {
         dy = -5;
     }
     else {
         dy = 5;
     }
-    x += dx;
-    y += dy;
+    ball.x += dx;
+    ball.y += dy;
 }
 
 function draw() {
     ctx.beginPath();
-    ctx.arc(x, y, 10, 0, Math.PI * 2);
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI * 2);
     ctx.stroke();
+}
+
+function gameOver() {
+
 }
